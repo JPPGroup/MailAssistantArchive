544e7b9b7e0ecbb9947f8b74d02c3bfad483e56e
jona <jon.abbott@jppuk.net>
2019-11-08 15:57:48 +0000

    wip

--- src/Factories/OutlookItemFactory.cs
+++ src/Factories/OutlookItemFactory.cs
@@ -27,20 +27,22 @@ namespace Jpp.AddIn.MailAssistant.Factories
 /*
  * Possible types to add later
  *
- * Outlook.AppointmentItem appointmentItem
- * Outlook.ContactItem contactItem
- * Outlook.DistListItem distListItem
- * Outlook.DocumentItem documentItem
- * Outlook.JournalItem journalItem
- * Outlook.NoteItem noteItem
- * Outlook.PostItem postItem
- * Outlook.RemoteItem remoteItem
- * Outlook.SharingItem sharingItem
- * Outlook.StorageItem storageItem
- * Outlook.TaskItem taskItem
- * Outlook.TaskRequestAcceptItem taskRequestAcceptItem 
- * Outlook.TaskRequestDeclineItem taskRequestDeclineItem
- * Outlook.TaskRequestItem taskRequestItem
- * Outlook.TaskRequestUpdateItem taskRequestUpdateItem
+ * https://docs.microsoft.com/en-us/office/vba/outlook/how-to/items-folders-and-stores/outlook-item-objects
+ *
+ * Outlook.AppointmentItem - Represents a meeting, a one-time appointment, or a recurring appointment or meeting in the Calendar folder.
+ * Outlook.ContactItem - Represents a contact in a Contacts folder.
+ * Outlook.DistListItem - Represents a distribution list in a Contacts folder.
+ * Outlook.DocumentItem - Represents any document other than a Microsoft Outlook item as an item in an Outlook folder.
+ * Outlook.JournalItem - Represents a journal entry in a Journal folder.
+ * Outlook.NoteItem - Represents a note in a Notes folder.
+ * Outlook.PostItem - Represents a post in a public folder that others may browse.
+ * Outlook.RemoteItem - Represents a remote item in an Inbox folder.
+ * Outlook.SharingItem - Represents a sharing message in an Inbox folder.
+ * Outlook.StorageItem - A message object in MAPI that is always saved as a hidden item in the parent folder and stores private data for Outlook solutions.
+ * Outlook.TaskItem - Represents a task (an assigned, delegated, or self-imposed task to be performed within a specified time frame) in a Tasks folder.
+ * Outlook.TaskRequestAcceptItem - Represents a response to a TaskRequestItem sent by the initiating user. 
+ * Outlook.TaskRequestDeclineItem - Represents a response to a TaskRequestItem sent by the initiating user.
+ * Outlook.TaskRequestItem - Represents a change to the recipient's Tasks list initiated by another party or as a result of a group tasking.
+ * Outlook.TaskRequestUpdateItem - Represents a response to a TaskRequestItem sent by the initiating user.
  */
 
